cbbe27741d0473580f11addf629b6bab843d5c7a
Saransh2705 <kukildeepak952@gmail.com>
2025-03-14 08:47:01 +0530

    Updated App component, main entry point, and Vite config

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Creatives from "./pages/Creatives";
+import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
